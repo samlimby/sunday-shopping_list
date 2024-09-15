70ff622b8e831af0fb6d2fb02ea696664f498d34
samlimby <129619263+samlimby@users.noreply.github.com>
2024-09-15 22:23:36 +0100

    component creation halfway

--- app.js
+++ app.js
@@ -19,19 +19,57 @@ function createAccordion() {
     const baseAccordion = document.createElement("div");
     baseAccordion.classList.add("accordion");
     baseAccordion.innerHTML = `
-        <p class="accordion_title">xxxxx</p>
-        <img src="icons/down-icon.png" id="accordion-chevron">
+        <div class="accordion-title_group">
+            <p class="accordion_title">xxxxx</p>
+            <img src="icons/down-icon.png" id="accordion-chevron" class="accordion-arrow">
+        </div>
     `;
     accordionSection.appendChild(baseAccordion);
 
     const accordionArrow = document.getElementById("accordion-chevron");
     accordionArrow.addEventListener("click", function(){
         console.log('pressed12')
-        removeAccordion(baseAccordion)
-    })
+        if (baseAccordion.classList.contains("accordion")) {
+            expandAccordion(baseAccordion)
+            accordionArrow.classList.replace("accordion-arrow", "accordion-arrow_rotate");
+        } else if (baseAccordion.classList.contains("accordion_active")) {
+            expandAccordion(baseAccordion)
+            accordionArrow.classList.replace("accordion-arrow_rotate", "accordion-arrow");
+        }
+    });
 }
 
 function removeAccordion(element) {
     element.remove()
 }
 
+function expandAccordion(element) {
+    if (element.classList.contains("accordion")) {
+        const accordionBody = document.createElement("div");
+        accordionBody.classList.add("accordion-body");
+        accordionBody.innerHTML = `
+            <div class="accordion-body_divider"><div>
+            <div class="accordion-item></div>
+        `;
+        element.appendChild(accordionBody);
+        const accordionItem = document.createElement("div");
+        accordionItem.classList.add("accordion-item");
+        accordionItem.innerHTML = `
+            <div class="unchecked"></div>
+            <p class="accordion-item_text">xxxx</p>
+        `;
+        accordionBody.appendChild(accordionItem);
+        element.classList.replace("accordion", "accordion_active");
+    } else {
+        console.log("invalid accordion action due to already being active!!!")
+    }
+};
+
+function collapseAccordion(element) {
+    if (element.classList.contains("accordion-active")) {
+        const accordionBody = document.querySelector("#accordion-body")
+        element.style.display = "hidden";
+    } else {
+        console.log("a")
+    }
+}
