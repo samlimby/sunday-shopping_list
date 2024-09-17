08fc6bd7ab2387fd6f429302f6351ac43574ad93
samlimby <129619263+samlimby@users.noreply.github.com>
2024-09-17 21:03:33 +0100

    fully functionality accordion expand and collapse interaction

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-// create and add content dynamically in the accordion with checkbox elements and have this be saved
-// add checkbox element button to the accordion
-// store the accordions in the array
-// set the date of accordion creation in the title of the accordion
-// ability to open and close the accordion 
-// save to localStorage and Firebase/supabase
 
 const createElement = document.getElementById("create-element"); 
 const accordionSection = document.getElementById("accordion_section");
 
 let accordionArray = [];
-let accordionStatus = {};
+let accordionStatus = {}; 
 
 createElement.addEventListener("click", function() {
     createAccordion();
@@ -21,7 +15,7 @@ function createAccordion() {
     accordion.classList.add("accordion");
     accordion.id = "accordion-item";
 
-    let accordionId = `accordion-chevron-${Date.now()}`;
+    let accordionId = `accordion-chevron-${Date.now()}`; 
 
     accordion.innerHTML = `
         <div class="accordion-title_group">
@@ -31,7 +25,7 @@ function createAccordion() {
     `;
     accordionSection.appendChild(accordion);
     accordionArray.push(accordion);
-    accordionStatus[accordionId] = false;
+    accordionStatus[accordionId] = false; 
     console.log(accordionArray);
     return accordion
 }
@@ -41,18 +35,46 @@ function updateAccordion() {
     for (let i = 0; i < accordionArray.length; i++) {
         const accordion = accordionArray[i];
         const accordionArrow = accordion.querySelector(".accordion-arrow");
+        let accordionItems = [];
 
         accordionArrow.addEventListener("click", function(){
             console.log("arrow selected")
             let id = accordionArrow.id;
-            if (accordionStatus[id]) {
+            if (accordionStatus[id]) {  
                 accordionArrow.classList.add("accordion-arrow");
                 accordionArrow.classList.remove("accordion-arrow_rotate");
-                accordionStatus[id] = false;
+                const accordionBody = accordion.querySelector("#accordion-container");
+
+                if (accordionBody) {
+                    accordionBody.remove();
+                }
+
+                accordion.classList.remove("accordion_active");
+                accordion.classList.add("accordion"); 
+                accordionStatus[id] = false; 
             } else {
                 accordionArrow.classList.add("accordion-arrow_rotate");
                 accordionArrow.classList.remove("accordion-arrow");
-                accordionStatus[id] = true;  
+                accordionStatus[id] = true; 
+                const accordionBody = document.createElement("div");
+                accordionBody.classList.add("accordion-body");
+                accordionBody.id = "accordion-container";
+                accordionBody.innerHTML = `
+                    <div id="accordion-divider" class="accordion-body_divider"><div>
+                    <div class="accordion-item></div>
+                `;
+                accordion.appendChild(accordionBody);
+                const accordionItem = document.createElement("div");
+                accordionItem.classList.add("accordion-item");
+                accordionItem.id = "accordion-item";
+                accordionItem.innerHTML = `
+                    <div class="unchecked"></div>
+                    <p class="accordion-item_text">xxxx</p>
+                `;
+                accordionBody.appendChild(accordionItem);
+                accordion.classList.remove("accordion"); 
+                accordion.classList.add("accordion_active");
+
             }
             console.log(accordionStatus)
         });
@@ -62,5 +84,12 @@ function updateAccordion() {
 updateAccordion()
 
 
+
+// accordion check box created through an input turning from placeholder to default
+// once an check box is turned from default from placeholder, another checkbox is created below
+// when an accordion check box is created push it into an accordionitems array
+// the value of an accordion items loops over the accordionitem array and creates as many items as there is length of the array
+// when the accordion is closed all the items are destroyed
+// accordionitem array is stored within the accordionstatus object under the specific property for the accordion as generated on line 18
   
 
